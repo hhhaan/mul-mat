0367f4b48d6bb93c0cc3ef5a9662049103b89480
hhhaan <hhanheon@gmail.com>
2025-04-08 16:48:00 +0900

    chore: dev환경 구분

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
-import { type NextRequest } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 import { updateSession } from '@/src/shared/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
+    if (process.env.NODE_ENV === 'development') {
+        return NextResponse.next();
+    }
     return await updateSession(request);
 }
 
