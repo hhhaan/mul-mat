ac837f1af524f23ebd2c99f66eec3c37fa212bc4
hhhaan <hhanheon@gmail.com>
2025-04-14 17:00:17 +0900

    fix: 쿠키 저장 로직 개선

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,27 +1,41 @@
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 // The client you created from the Server-Side Auth instructions
 import { createClient } from '@/src/shared/utils/supabase/server';
+import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url);
     const code = searchParams.get('code');
     // if "next" is in param, use it as the redirect URL
-    const next = searchParams.get('next') ?? '/';
+    // const next = searchParams.get('next') ?? '/';
+
+    // 테스트용
+    const next = '/';
 
     if (code) {
         const supabase = await createClient();
         const { error } = await supabase.auth.exchangeCodeForSession(code);
         if (!error) {
-            const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
-            const isLocalEnv = process.env.NODE_ENV === 'development';
-            if (isLocalEnv) {
-                // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-                return NextResponse.redirect(`${origin}${next}`);
-            } else if (forwardedHost) {
-                return NextResponse.redirect(`https://${forwardedHost}${next}`);
-            } else {
-                return NextResponse.redirect(`${origin}${next}`);
-            }
+            // 세션이 설정된 후 리다이렉트
+            const response = NextResponse.redirect(new URL(next, origin));
+
+            // 쿠키를 새로운 응답에 복사
+            const cookieStore = await cookies();
+            const allCookies = cookieStore.getAll();
+            allCookies.forEach((cookie: ResponseCookie) => {
+                response.cookies.set(cookie.name, cookie.value, {
+                    domain: cookie.domain,
+                    expires: cookie.expires,
+                    httpOnly: cookie.httpOnly,
+                    maxAge: cookie.maxAge,
+                    path: cookie.path,
+                    sameSite: cookie.sameSite,
+                    secure: cookie.secure,
+                });
+            });
+
+            return response;
         }
     }
 
