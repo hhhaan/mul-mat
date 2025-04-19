49a97390da93f2affd5ef89afce4f31f0e7fd421
hhhaan <hhanheon@gmail.com>
2025-04-20 04:12:53 +0900

    refactor: spinner 컴포넌트 분리

---
+++ src/shared/ui/spinner/spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+    return (
+        <div className="flex items-center justify-center h-64">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-cyan-400"></div>
+        </div>
+    );
+};
