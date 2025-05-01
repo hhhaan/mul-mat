a2aeb664b4c3e19be96700f89b63eb1944c2c0f0
hhhaan <hhanheon@gmail.com>
2025-05-01 19:06:42 +0900

    chore: 사용하지 않는 로그인 방법 삭제

--- src/widgets/login-form/index.tsx
+++ src/widgets/login-form/index.tsx
@@ -4,7 +4,7 @@ import { Droplets } from 'lucide-react';
 
 import { KakaoLoginButton } from '@/src/features/auth/ui/kakao-login-button';
 
-export const LoginWidgets = () => {
+export const LoginForm = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-50 to-white p-4">
             {/* Logo and App Name */}
@@ -28,16 +28,16 @@ export const LoginWidgets = () => {
                 </div>
 
                 <div className="relative my-6">
-                    <div className="absolute inset-0 flex items-center">
+                    {/* <div className="absolute inset-0 flex items-center">
                         <div className="w-full border-t border-gray-300"></div>
-                    </div>
-                    <div className="relative flex justify-center text-sm">
+                    </div> */}
+                    {/* <div className="relative flex justify-center text-sm">
                         <span className="px-2 bg-white text-gray-500">또는</span>
-                    </div>
+                    </div> */}
                 </div>
 
                 {/* Other Login Options (Placeholder) */}
-                <div className="space-y-3">
+                {/* <div className="space-y-3">
                     <button
                         className="w-full flex items-center justify-center py-3 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
                         onClick={() => {
@@ -46,13 +46,14 @@ export const LoginWidgets = () => {
                     >
                         다른 방법으로 로그인
                     </button>
-                </div>
+                </div> */}
             </div>
 
-            {/* Footer */}
+            {/* 푸터 */}
             <div className="text-center text-gray-500 text-xs">
                 <p>
-                    로그인하면{' '}
+                    로그인하면 이용약관 및 개인정보처리방침에 동의하게 됩니다.
+                    {/* 로그인하면{' '}
                     <a href="#" className="text-blue-600">
                         이용약관
                     </a>{' '}
@@ -60,7 +61,7 @@ export const LoginWidgets = () => {
                     <a href="#" className="text-blue-600">
                         개인정보처리방침
                     </a>
-                    에 동의하게 됩니다.
+                    에 동의하게 됩니다. */}
                 </p>
                 <p className="mt-2">&copy; 2025 mulmat. All rights reserved.</p>
             </div>
